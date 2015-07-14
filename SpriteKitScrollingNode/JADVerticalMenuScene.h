1577b9f3c3103a12d2f72616a8b90126b0a668a1
Iris <iz2140@columbia.edu>
2015-07-14 18:19:39 -0400

    Add new vertical menu scene

---
+++ SpriteKitScrollingNode/JADVerticalMenuScene.h
@@ -0,0 +1,13 @@
+//
+//  JADVerticalMenuScene.h
+//  SpriteKitScrollingNode
+//
+//  Created by Iris Zhang on 7/9/15.
+//  Copyright (c) 2015 Jennifer Dobson. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface JADVerticalMenuScene : SKScene
+
+@end
---
+++ SpriteKitScrollingNode/JADVerticalMenuScene.m
@@ -0,0 +1,13 @@
+//
+//  JADVerticalMenuScene.m
+//  SpriteKitScrollingNode
+//
+//  Created by Iris Zhang on 7/9/15.
+//  Copyright (c) 2015 Jennifer Dobson. All rights reserved.
+//
+
+#import "JADVerticalMenuScene.h"
+
+@implementation JADVerticalMenuScene
+
+@end
